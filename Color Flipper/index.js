00e10271f57b9dbf1d443b740ab4b9a398689ec5
Gattskii <117891948+Gattskii@users.noreply.github.com>
2025-07-01 12:48:10 +0700

    finished Color Flipper

--- Color Flipper/index.js
+++ Color Flipper/index.js
@@ -4,4 +4,13 @@ function setColor(color) {
     body.style.backgroundColor = color;
 }
 
-setColor('blue');
+function setRandomColor() {
+    let r = Math.floor(Math.random() * 256);
+    let g = Math.floor(Math.random() * 256);
+    let b = Math.floor(Math.random() * 256);
+
+    const color = `rgb(${r}, ${g}, ${b})`;
+
+    console.log(color);
+    body.style.backgroundColor = color;
+}
